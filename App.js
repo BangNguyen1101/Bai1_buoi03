ddfa694cb4a6f28884cdbac312c497d3db0c3df6
BangNguyen1101 <133348624+BangNguyen1101@users.noreply.github.com>
2025-01-07 19:23:46 +0700

    upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.square}>
+        <Text style={styles.text}>Hello, world</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  square: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'pink',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: 'black',
+  },
+});
+
+export default App;
